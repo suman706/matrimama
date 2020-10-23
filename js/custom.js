5a74fef4e094cedf22c7965ea9342dcf9b63bf5c
RAHUL KUMAR <68408499+suman706@users.noreply.github.com>
2020-10-23 10:52:12 +0530

    update

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+function register(){
+	var regist = document.getElementById("register");
+	regist.style.display="none";
+
+	var regist_menu = document.getElementById("register_menu");
+	regist_menu.style.display="none";
+
+	var photo_menu = document.getElementById("photo_menu");
+	photo_menu.style.display="block";
+
+	var x = document.getElementById("photo_upload")
+	x.style.display="block";
+}
+	var photo_menu = document.getElementById("photo_menu");
+	photo_menu.style.display="none";
+
+	var phot = document.getElementById("photo_upload");
+	phot.style.display="none";
+
+function register_page(){
+	var regist = document.getElementById("register");
+	regist.style.display="block";
+
+	var regist_menu = document.getElementById("register_menu");
+	regist_menu.style.display="block";
+
+	var photo_meu = document.getElementById("photo_menu");
+	photo_menu.style.display="none";
+
+	var x = document.getElementById("photo_upload")
+	x.style.display="none";
+}
