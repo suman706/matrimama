0a5f6bab493a48ad8250209e14c56b7509861ea4
RAHUL KUMAR <68408499+suman706@users.noreply.github.com>
2020-11-06 15:02:20 +0530

    update

--- js/custom.js
+++ js/custom.js
@@ -41,6 +41,12 @@
 
 	var lifestyle_form = document.getElementById("lifestyle_form");
 	lifestyle_form.style.display = "none";
+
+	var partner_menu = document.getElementById("partner_menu");
+	partner_menu.style.display = "none";
+
+	var partner_form = document.getElementById("partner_form");
+	partner_form.style.display = "none";
 //======================================================================
 //======================================================================
 
