c50dff746b4287c9a1a789fa33ca2ebf22243e04
RAHUL KUMAR <68408499+suman706@users.noreply.github.com>
2020-11-15 08:39:12 +0530

    update

--- js/custom1.js
+++ js/custom1.js
@@ -1,5 +1,6 @@
 var menu = document.getElementsByClassName("register_process_menu");
 var register = document.getElementsByClassName("register_process");
+var mobile = document.getElementsByClassName("mobile_menu");
 
 //console.log(menu[0].style.display  + "\n" + register);
 function start_with_hide(){
@@ -7,6 +8,7 @@ function start_with_hide(){
 		//console.log(i);
 		menu[i].style.display = "none";		
 		register[i].style.display = 'none';
+		mobile[i].style.display = 'none';
 	}
 }
 
@@ -16,6 +18,7 @@ start_with_hide();
 //displaying first blcok in starting
 menu[0].style.display = "block";
 register[0].style.display = "block";
+mobile[0].style.display = "block";
 
 //next button function
 function next_form(counter,event){
@@ -23,23 +26,29 @@ function next_form(counter,event){
 	console.log(counter + "\n");
 	menu[counter].style.display = "none";
 	register[counter].style.display = "none";
+	mobile[counter].style.display = "none";
 	menu[counter + 1].style.display = "block";
 	register[counter + 1].style.display = "block";
+	mobile[counter + 1].style.display = "block";
 	console.log(register[counter+1].style.display);
 }
 
-//next button function
+//prev button function
 function prev_form(counter){
 	menu[counter].style.display = "none";
 	register[counter].style.display = "none";
+	mobile[counter].style.display = "none";
 	menu[counter - 1].style.display = "block";
 	register[counter - 1].style.display = "block";
+	mobile[counter - 1].style.display = "block";
 }
 
 //skip button function
 function skip_form(counter){
 	menu[counter].style.display = "none";
 	register[counter].style.display = "none";
+	mobile[counter].style.display = "none";
 	menu[counter + 1].style.display = "block";
 	register[counter + 1].style.display = "block";
+	mobile[counter + 1].style.display = "block";
 }
